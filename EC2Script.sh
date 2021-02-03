2d584572482ad31448248bfb674ac77ab4e20f9a
Thomas Auer <thauer@edu.aau.at>
2021-02-03 20:35:59 +0100

    Testing on EC2-Servs

---
+++ EC2Script.sh
@@ -0,0 +1,7 @@
+#bin/bash
+sudo apt-get install default-jdk nginx
+
+git clone https://github.com/AAU-WeatherReporterProject/WeatherReporterBackend.git
+git clone https://github.com/AAU-WeatherReporterProject/FrontendWebsite.git
+
+
