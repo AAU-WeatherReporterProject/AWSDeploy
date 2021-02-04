71ca3a9646aefd7f4919c3ed54013e4f8a81a167
Thomas Auer <thauer@edu.aau.at>
2021-02-04 12:55:14 +0100

    Webpage Deployment

--- EC2Script.sh
+++ EC2Script.sh
@@ -5,7 +5,10 @@ sudo apt-get install default-jdk nginx -y
 git clone https://github.com/AAU-WeatherReporterProject/WeatherReporterBackend.git
 git clone https://github.com/AAU-WeatherReporterProject/FrontendWebsite.git
 
-mv WeatherReporterBackend/src/main/java/at/aau/projects/weatherreporter/rest/jar/weather-reporter-0.0.1-SNAPSHOT.jar ./
+sudo mv FrontendWebsite/ /var/www/html/
+sed 's,/var/www/html,/var/www/html/FrontendWebsite/target/dist' /etc/nginx/sites-available/default
+sudo systemctl restart nginx
 
+sudo mv WeatherReporterBackend/src/main/java/at/aau/projects/weatherreporter/rest/jar/weather-reporter-0.0.1-SNAPSHOT.jar ./
 sudo /usr/bin/java -jar weather-reporter-0.0.1-SNAPSHOT.jar
 
