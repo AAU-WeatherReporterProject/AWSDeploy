a55e3937e03d0635b5087aa9f9de709c9aba4f0a
Thomas Auer <thauer@edu.aau.at>
2021-02-04 13:53:59 +0100

    Typo #3

--- EC2Script.sh
+++ EC2Script.sh
@@ -7,7 +7,7 @@ git clone https://github.com/AAU-WeatherReporterProject/FrontendWebsite.git
 
 sudo mv FrontendWebsite/ /var/www/html/
 sudo sed -i 's,/var/www/html,/var/www/html/FrontendWebsite/target/dist,g' /etc/nginx/sites-available/default
-sudo sed -i 's,/var/www/html/var/www/html/FrontendWebsite/target/dist/var/www/html/FrontendWebsite/target/dist,/var/www/html/FrontendWebsite/target/dist,g' /etc/nginx/sites-available/default
+sudo sed -i 's,/var/www/html/FrontendWebsite/target/dist/FrontendWebsite/target/dist,/var/www/html/FrontendWebsite/target/dist,g' /etc/nginx/sites-available/default
 sudo systemctl restart nginx
 
 sudo mv WeatherReporterBackend/src/main/java/at/aau/projects/weatherreporter/rest/jar/weather-reporter-0.0.1-SNAPSHOT.jar ./
