399c5afb5cf4eb899991ab7eeeb0af015cf67f89
Thomas Auer <thauer@edu.aau.at>
2021-02-04 13:03:56 +0100

    Typo in sed

--- EC2Script.sh
+++ EC2Script.sh
@@ -6,7 +6,7 @@ git clone https://github.com/AAU-WeatherReporterProject/WeatherReporterBackend.g
 git clone https://github.com/AAU-WeatherReporterProject/FrontendWebsite.git
 
 sudo mv FrontendWebsite/ /var/www/html/
-sed 's,/var/www/html,/var/www/html/FrontendWebsite/target/dist' /etc/nginx/sites-available/default
+sudo sed 's,/var/www/html,/var/www/html/FrontendWebsite/target/dist,g' /etc/nginx/sites-available/default
 sudo systemctl restart nginx
 
 sudo mv WeatherReporterBackend/src/main/java/at/aau/projects/weatherreporter/rest/jar/weather-reporter-0.0.1-SNAPSHOT.jar ./
