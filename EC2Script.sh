2a39cddb57655c397a57ec116828237fb4cc0c71
Thomas Auer <thauer@edu.aau.at>
2021-02-04 14:49:39 +0100

    Start as process

--- EC2Script.sh
+++ EC2Script.sh
@@ -11,5 +11,5 @@ sudo sed -i 's,/var/www/html/FrontendWebsite/target/dist/FrontendWebsite/target/
 sudo systemctl restart nginx
 
 sudo mv WeatherReporterBackend/src/main/java/at/aau/projects/weatherreporter/rest/jar/weather-reporter-0.0.1-SNAPSHOT.jar ./
-sudo /usr/bin/java -jar weather-reporter-0.0.1-SNAPSHOT.jar
+sudo /usr/bin/java -jar weather-reporter-0.0.1-SNAPSHOT.jar &
 
